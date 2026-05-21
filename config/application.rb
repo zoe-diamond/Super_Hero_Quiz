fee2f211ec537528581f881a0d18a9dd0183b46a
Zoe Diamond <zoediamond@meta.com>
2026-05-21 15:07:55 -0400

    Modernize to Rails 8 and Ruby 3.2+

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,8 @@ Bundler.require(*Rails.groups)
 
 module SuperHeroQuiz
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    # Initialize configuration defaults for Rails 8.1.
+    config.load_defaults 8.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
